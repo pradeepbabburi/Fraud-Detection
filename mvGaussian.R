54cf80e1e5739505d191e119d7e2d4520c45ead3
pbabburi <pradeep.b1489@gmail.com>
2016-12-04 17:34:04 -0600

    Updated function mvGaussian

--- mvGaussian.R
+++ mvGaussian.R
@@ -1,11 +1,12 @@
-mvGaussian <- function(X, mu, sigma) {
-  k = length(mu)
-  X = X - mu
-  C = (2 * pi)^(-k / 2)*det(sigma)^(-0.5)
+mvGaussian <- function(x, mu, sigma) {
+  k <- length(mu)
+  x <- x - mu
+  x <- data.matrix(x)
+  C <- (2 * pi)^(-k / 2)*det(sigma)^(-0.5)
   sigma.inv <- solve(sigma)
   tmp <- NULL
-  for (i in 1:nrow(X)) {
-    tmp <- c(tmp, exp(-0.5 * t(X[i,]) %*% sigma.inv %*% matrix(X[i,])))
-  }
+  #for (i in 1:nrow(X)) {
+    tmp <- exp(-0.5 * x %*% sigma.inv %*% t(x))
+  #}
   return(C*tmp)
 }
