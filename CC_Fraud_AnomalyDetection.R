3597baa70da2e9dc7c20687ec0b3dab67311e392
pbabburi <pradeep.b1489@gmail.com>
2017-03-14 19:57:23 -0500

    Updated model

--- CC_Fraud_AnomalyDetection.R
+++ CC_Fraud_AnomalyDetection.R
@@ -2,6 +2,7 @@ library(data.table)
 library(psych)
 library(mvtnorm)
 library(caret)
+library(PRROC)
 source('selectThreshold.R')
 
 # read data set
@@ -35,35 +36,54 @@ Xanom[, todrop] <- NULL
 multi.hist(Xgood[,c(2,3,5,9)], density = T)
 
 # split the data 60/20/20 into train/cv/test set with 0/50/50 anomalous examples 
-g <- nrow(Xgood); a <- nrow(Xanom)
-rg <- sample(1:g, g, replace = F)
-ra <- sample(1:a, a, replace = F)
+g <- rownames(Xgood); a <- rownames(Xanom);
+lg <- length(g); la <- length(a);
 # training set
-Xtrain <- data.matrix(Xgood[rg[1:(g*0.6)],-c(1,17)])
+Xtrain <- data.matrix(Xgood[g[1:(lg*0.6)],-c(1,17)])
 # cross validation set
-Xcv <- data.matrix(rbind(Xgood[rg[(g*0.6+1):(g*0.8)],-c(1,17)], Xanom[ra[1:(a*0.5)],-c(1,17)]))
+Xcv <- data.matrix(rbind(Xgood[g[(lg*0.6+1):(lg*0.8)],-c(1,17)], Xanom[a[1:(la*0.5)],-c(1,17)]))
 # test set
-Xtest <- data.matrix(rbind(Xgood[rg[(g*0.8+1):g],-c(1,17)], Xanom[ra[(a*0.5 + 1):a],-c(1,17)]))
+Xtest <- data.matrix(rbind(Xgood[g[(lg*0.8+1):lg],-c(1,17)], Xanom[a[(la*0.5+1):la],-c(1,17)]))
 # shuffle the data
 Xcv <- Xcv[sample(nrow(Xcv), nrow(Xcv), replace = F),]
-Xtest <- Xcv[sample(nrow(Xtest), nrow(Xtest), replace = F),]
+Xtest <- Xtest[sample(nrow(Xtest), nrow(Xtest), replace = F),]
 # cleanup variables that are not required
-rm(list = c("a", "g", "ra", "rg", "todrop"))
+rm(list = c("a", "g", "la", "lg", "todrop"))
 
 # esimate gaussian
-mu <- apply(Xtrain[,-16], 2, mean)       # mean of all features
-sigma <- cov(Xtrain[,-16])               # covariance matrix of all features
+class = 16                               # column position of class variable
+mu <- apply(Xtrain[,-class], 2, mean)       # mean of all features
+sigma <- cov(Xtrain[,-class])               # covariance matrix of all features
 #sigma <- apply(Xtrain[,-16], 2, var)    
 #sigma <- diag(sigma)                      # assuming all the features are uncorrelated
 
 # find the pdf on cross validation set
-pcv <- dmvnorm(Xcv[,-16], mu, sigma)
+pcv <- dmvnorm(Xcv[,-class], mu, sigma)
+pcv[pcv==0] <- min(pcv[pcv>0])
+pcv <- log(pcv)
+
 # find the best threshold for maximum F1 score
-epsilon <- selectThreshold(Xcv[,16], pcv)
+epsilon <- selectThreshold(Xcv[,class], pcv)
 sprintf('The threshold value is found to be epsilon = %e', epsilon)
+pred_cv <- as.numeric(pcv < epsilon)
+confusionMatrix(table(pred_cv, Xcv[,class]), 
+                positive = levels(factor(Xcv[,class]))[2], 
+                mode = "prec_recall")
 
 # evaluate the model on test set
-ptest <- dmvnorm(Xtest[,-16], mu, sigma)
-prediction <- as.numeric(ptest < epsilon)
-confusionMatrix(table(Xtest[,16], prediction), positive = levels(factor(Xtest[,16]))[2])
+ptest <- dmvnorm(Xtest[,-class], mu, sigma)
+ptest[ptest==0] <- min(ptest[ptest>0])
+ptest <- log(ptest)
+pred_test <- as.numeric(ptest < epsilon)
+confusionMatrix(table(pred_test, Xtest[,class]), 
+                positive = levels(factor(Xtest[,class]))[2], 
+                mode = "prec_recall")
+# plot PR curve
+fg <- pred_test[Xtest[,class]==1]
+bg <- pred_test[Xtest[,class]==0]
+pr <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
+plot(pr)
 
+# model analysis
+min(ptest)
+sum(names(ptest[ptest==min(ptest)]) %in% rownames(X[X$Class==0,]))
--- selectThreshold.R
+++ selectThreshold.R
@@ -1,32 +1,30 @@
 selectThreshold <- function(ycv, pcv)
 {
-# initiate variales
-bestEpsilon <- 0; epsilon <- 0; bestF1 <- 0; F1 <- 0; 
-stepsize = (max(pcv) - min(pcv)) / 10000000
-
-# loop through a range of epsilon values
-for (epsilon in seq(min(pcv)+stepsize, max(pcv), stepsize)) {
-  pred <- as.numeric(pcv < epsilon)
-  # calculate precision and recall
-  tp <- sum((pred == 1) & (ycv == 1))   # true positives
-  fp <- sum((pred == 1) & (ycv == 0))   # false positives
-  fn <- sum((pred == 0) & (ycv == 1))   # false negatives
-
-  #if (tp + fp == 0) prec <- 0 else 
-  prec <- tp/(tp + fp); rec <- tp/(tp + fn);
-  
-  # calculate F score
-  #if (prec + rec == 0) F1 <- 0 else 
-  F1 <- (2 * prec * rec)/(prec + rec)
-  # find best epsilon
-  if (F1 > bestF1) {
-    bestF1 <- F1
-    bestEpsilon <- epsilon
-    #message(sprintf('best f1: %e, best epsilon: %e\n', bestF1, bestEpsilon))
-    loopct <- 1
-  } 
-  loopct = loopct+1
-  if (loopct > 10000) break
-}
-return(bestEpsilon)
+  # initiate variales
+  bestEpsilon <- 0; epsilon <- 0; bestF1 <- 0; F1 <- 0; 
+  stepsize <- (max(pcv) - min(pcv)) / 1000
+  # loop through a range of epsilon values
+  for (epsilon in seq(min(pcv)+stepsize, max(pcv), stepsize)) {
+    pred <- as.numeric(pcv < epsilon)
+    # calculate precision and recall
+    tp <- sum((pred == 1) & (ycv == 1))   # true positives
+    fp <- sum((pred == 1) & (ycv == 0))   # false positives
+    fn <- sum((pred == 0) & (ycv == 1))   # false negatives
+    
+    prec <- tp/(tp + fp)  # precision
+    rec <- tp/(tp + fn)   # recall
+    
+    # calculate F1 score
+    F1 <- (2 * prec * rec)/(prec + rec)
+    # find best epsilon
+    if (F1 > bestF1) {
+      bestF1 <- F1
+      bestEpsilon <- epsilon
+      #message(sprintf('best f1: %e, best epsilon: %e\n', bestF1, bestEpsilon))
+      loopct <- 1
+    } 
+    loopct = loopct+1
+    if (loopct > 10000) break
+  }
+  return(bestEpsilon)
 }
